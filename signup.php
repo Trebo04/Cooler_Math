6d29293342bacf1caefccfdc56cfa7fbff654ab8
Trebo04 <jonathan.janda@outlook.com>
2023-04-30 21:27:53 -0500

    fixed scale for mobile on signup

--- signup.php
+++ signup.php
@@ -101,6 +101,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             background-color: #252525
         }
     </style>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" href="icon.png" />
+    <link rel="apple-touch-icon" sizes="128x128" href="high_def_icon.png" />
     <link href="main.css" rel="stylesheet" />
     <title>Signup | Cooler Math</title>
 </head>
